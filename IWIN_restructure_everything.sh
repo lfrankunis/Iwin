1c5f471bed82d4b177be63c9eecac6d64a6ca59b
lfrankunis <lukasf@unis.no>
2023-01-26 07:39:18 +0000

    data transfer to MET

--- IWIN_restructure_everything.sh
+++ IWIN_restructure_everything.sh
@@ -1,10 +1,11 @@
 source /home/unismet/miniconda3/etc/profile.d/conda.sh
 conda activate iwin
 cd /mnt/c/Github/Iwin/
-python ./IWIN_manual_restructuring.py -s 1884 -t 20220617 20221220
-python ./IWIN_manual_restructuring.py -s 1885 -t 20210819 20221220
-python ./IWIN_manual_restructuring.py -s 1886 -t 20220708 20221220
-python ./IWIN_manual_restructuring.py -s 1887 -t 20220903 20221220
+ENDDAY=$(date -d "-2 day" "+ %Y%m%d")
+python ./IWIN_manual_restructuring.py -s 1884 -t 20220617 "$ENDDAY"
+python ./IWIN_manual_restructuring.py -s 1885 -t 20210819 "$ENDDAY"
+python ./IWIN_manual_restructuring.py -s 1886 -t 20220708 "$ENDDAY"
+python ./IWIN_manual_restructuring.py -s 1887 -t 20220903 "$ENDDAY"
 
 python ./IWIN_manual_restructuring.py -s 1883 -t 20210505 20211021
 python ./IWIN_manual_restructuring.py -s 1872 -t 20210528 20211020
--- IWIN_scheduled_restructuring.sh
+++ IWIN_scheduled_restructuring.sh
@@ -2,3 +2,5 @@ source /home/unismet/miniconda3/etc/profile.d/conda.sh
 conda activate iwin
 cd /mnt/c/Github/Iwin/
 python ./IWIN_scheduled_restructuring.py
+python ./IWIN_copy_files_for_transfer_MET.py 5
+rsync -avh --delete /mnt/c/Data_transfer_MET unismet@157.249.75.166:/data/unismet/iwin
