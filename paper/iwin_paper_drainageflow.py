3eba472a85769d5ada48a7bd5f619a1982c6a0ba
lfrankunis <lukasf@unis.no>
2023-02-21 09:14:04 +0100

    drainage flow place names

--- paper/iwin_paper_drainageflow.py
+++ paper/iwin_paper_drainageflow.py
@@ -97,8 +97,8 @@ times_drainage_signals = np.array([ pd.Timestamp("2022-10-20 07:57"),
                                     pd.Timestamp("2022-10-20 10:04"),
                                     pd.Timestamp("2022-10-20 11:07")], dtype="datetime64")
 
-wind_arrow_data = boat_data.sel(time = times_drainage_signals)
-lighthouse_arrow_data = lighthouse_data.sel(time = times_drainage_signals)
+# wind_arrow_data = boat_data.sel(time = times_drainage_signals)
+# lighthouse_arrow_data = lighthouse_data.sel(time = times_drainage_signals)
 
 fig, ax = plt.subplots(1,1, figsize=latex_helpers.set_size(503.6, whr=0.7), subplot_kw={'projection': ccrs.Mercator()})
 ax.set_xticks([15.5, 16., 16.5, 17.], crs=ccrs.PlateCarree())
@@ -120,26 +120,36 @@ ax.scatter([15.63083], [78.22433], color="k", marker='d', s=50, transform=ccrs.P
 # PYR
 ax.scatter([16.33432], [78.65312], color="k", marker='^', s=50, transform=ccrs.PlateCarree(), zorder=100)
 
-df = pd.DataFrame({'latitude': boat_data["latitude"], 'longitude': boat_data["longitude"], "color": boat_data["temperature_Avg"]})
-gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude), crs="EPSG:4326")
-gdf = gdf.to_crs(ccrs.Mercator().proj4_init)
-pic = ax.scatter(x=gdf["longitude"], y=gdf["latitude"], c=gdf["color"], s=1, zorder=100, cmap=cmap, transform=ccrs.PlateCarree(), vmin=vmin, vmax=vmax)
-cbar = plt.colorbar(pic, ax=ax, orientation="vertical")
-cbar.ax.set_ylabel("Temperature [°C]")
+# location names
+ax.annotate("\\bf Kapp Ekholm", (16.58, 78.575), xycoords=ccrs.PlateCarree()._as_mpl_transform(ax), c="k", weight="bold", fontsize=8, rotation=0., zorder=1000.)
+ax.annotate("\\bf Skansbukta", (15.65, 78.53), xycoords=ccrs.PlateCarree()._as_mpl_transform(ax), c="k", weight="bold", fontsize=8, rotation=0., zorder=1000.)
+ax.annotate("\\bf Rundodden", (15.7, 78.51), xycoords=ccrs.PlateCarree()._as_mpl_transform(ax), c="k", weight="bold", fontsize=8, rotation=0., zorder=1000.)
+ax.annotate("\\bf Narveneset", (16., 78.58), xycoords=ccrs.PlateCarree()._as_mpl_transform(ax), c="k", weight="bold", fontsize=8, rotation=0., zorder=1000.)
+ax.annotate("\\bf Pyramiden", (15.98, 78.65), xycoords=ccrs.PlateCarree()._as_mpl_transform(ax), c="k", weight="bold", fontsize=8, rotation=0., zorder=1000.)
+ax.annotate("\\bf Nordenskiöldbreen", (17., 78.63), xycoords=ccrs.PlateCarree()._as_mpl_transform(ax), c="k", weight="bold", fontsize=8, rotation=70., zorder=1000.)
+ax.annotate("\\bf Gåsøyane", (16.135, 78.435), xycoords=ccrs.PlateCarree()._as_mpl_transform(ax), c="k", weight="bold", fontsize=8, rotation=0., zorder=1000.)
 
-u = -np.abs(wind_arrow_data["wind_speed_corrected_Avg"]) * np.sin(np.deg2rad(wind_arrow_data["wind_direction_corrected_Avg"]))
-v = -np.abs(wind_arrow_data["wind_speed_corrected_Avg"]) * np.cos(np.deg2rad(wind_arrow_data["wind_direction_corrected_Avg"]))
-df = pd.DataFrame({'latitude': wind_arrow_data["latitude"], 'longitude': wind_arrow_data["longitude"], "u": 1.94384*u, "v": 1.94384*v})
-gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude), crs="EPSG:4326")
-gdf = gdf.to_crs(ccrs.Mercator().proj4_init)
-ax.barbs(gdf['geometry'].x, gdf['geometry'].y, gdf['u'], gdf['v'], wind_arrow_data.time.values.astype(float), length=6., linewidth=1., cmap=cmap_barbs, zorder=130)
 
-u = -np.abs(lighthouse_arrow_data["wind_speed_Avg"]) * np.sin(np.deg2rad(lighthouse_arrow_data["wind_direction_Avg"]))
-v = -np.abs(lighthouse_arrow_data["wind_speed_Avg"]) * np.cos(np.deg2rad(lighthouse_arrow_data["wind_direction_Avg"]))
-df = pd.DataFrame({'latitude': np.ones_like(lighthouse_arrow_data["wind_speed_Avg"].values)*78.45792, 'longitude': np.ones_like(lighthouse_arrow_data["wind_speed_Avg"].values)*16.20082, "u": 1.94384*u, "v": 1.94384*v})
-gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude), crs="EPSG:4326")
-gdf = gdf.to_crs(ccrs.Mercator().proj4_init)
-ax.barbs(gdf['geometry'].x, gdf['geometry'].y, gdf['u'], gdf['v'], lighthouse_arrow_data.time.values.astype(float), length=6., linewidth=1., cmap=cmap_barbs, zorder=430)
+# df = pd.DataFrame({'latitude': boat_data["latitude"], 'longitude': boat_data["longitude"], "color": boat_data["temperature_Avg"]})
+# gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude), crs="EPSG:4326")
+# gdf = gdf.to_crs(ccrs.Mercator().proj4_init)
+# pic = ax.scatter(x=gdf["longitude"], y=gdf["latitude"], c=gdf["color"], s=1, zorder=100, cmap=cmap, transform=ccrs.PlateCarree(), vmin=vmin, vmax=vmax)
+# cbar = plt.colorbar(pic, ax=ax, orientation="vertical")
+# cbar.ax.set_ylabel("Temperature [°C]")
+
+# u = -np.abs(wind_arrow_data["wind_speed_corrected_Avg"]) * np.sin(np.deg2rad(wind_arrow_data["wind_direction_corrected_Avg"]))
+# v = -np.abs(wind_arrow_data["wind_speed_corrected_Avg"]) * np.cos(np.deg2rad(wind_arrow_data["wind_direction_corrected_Avg"]))
+# df = pd.DataFrame({'latitude': wind_arrow_data["latitude"], 'longitude': wind_arrow_data["longitude"], "u": 1.94384*u, "v": 1.94384*v})
+# gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude), crs="EPSG:4326")
+# gdf = gdf.to_crs(ccrs.Mercator().proj4_init)
+# ax.barbs(gdf['geometry'].x, gdf['geometry'].y, gdf['u'], gdf['v'], wind_arrow_data.time.values.astype(float), length=6., linewidth=1., cmap=cmap_barbs, zorder=130)
+
+# u = -np.abs(lighthouse_arrow_data["wind_speed_Avg"]) * np.sin(np.deg2rad(lighthouse_arrow_data["wind_direction_Avg"]))
+# v = -np.abs(lighthouse_arrow_data["wind_speed_Avg"]) * np.cos(np.deg2rad(lighthouse_arrow_data["wind_direction_Avg"]))
+# df = pd.DataFrame({'latitude': np.ones_like(lighthouse_arrow_data["wind_speed_Avg"].values)*78.45792, 'longitude': np.ones_like(lighthouse_arrow_data["wind_speed_Avg"].values)*16.20082, "u": 1.94384*u, "v": 1.94384*v})
+# gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude), crs="EPSG:4326")
+# gdf = gdf.to_crs(ccrs.Mercator().proj4_init)
+# ax.barbs(gdf['geometry'].x, gdf['geometry'].y, gdf['u'], gdf['v'], lighthouse_arrow_data.time.values.astype(float), length=6., linewidth=1., cmap=cmap_barbs, zorder=430)
 
 
 ax.set_extent(lon_lims+lat_lims, crs=ccrs.PlateCarree())
